190330f53068b1ff89e3dd8ba8b8821ba59b8606
Kawan Dias Carneiro <kawandiaskdc@gmail.com>
2026-08-10 13:21:26 -0300

    fix: arrumando problema do NODE e versao

--- api/search.js
+++ api/search.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36';
